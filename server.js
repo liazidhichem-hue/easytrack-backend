f9bd7d0d7b7d549b9438221e893e0877d4f4ece6
liazidhichem-hue <liazidhichem@gmail.com>
2026-06-10 01:19:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import cors from "cors";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors());app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store');
+  next();
+});
 app.use(express.json());
 
 // ✅ Yalidine
